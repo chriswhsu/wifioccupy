93777b9c02c8452ad91499152c104f5796f6f0fb
ChrisHsu <chris.w.hsu@gmail.com>
2020-08-21 12:00:32 -0700

    start building data model.

--- datapoll/models.py
+++ datapoll/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MACAddress(models.Model):
+
+    mac_address = models.CharField(max_length=17, unique=True, null=False)
+    ip_address = models.CharField(max_length=15)
+    hostname = models.CharField(max_length=30)
+    alternative_name = models.CharField(max_length=30)
--- wifioccupy/settings.py
+++ wifioccupy/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -28,10 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'datapoll',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -71,16 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wifioccupy.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from wifioccupy.sensative_settings import THE_DATABASES
+DATABASES = THE_DATABASES
 
 
 # Password validation
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
