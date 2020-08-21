7ca4f17a84c5563eb9d54c9146ee1fed797400b0
ChrisHsu <chris.w.hsu@gmail.com>
2020-08-21 12:58:36 -0700

    start building additional models.

--- datapoll/models.py
+++ datapoll/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class MACAddress(models.Model):
-
     mac_address = models.CharField(max_length=17, unique=True, null=False)
     ip_address = models.CharField(max_length=15)
     hostname = models.CharField(max_length=30)
     alternative_name = models.CharField(max_length=30)
+
+
+class Router(models.Model):
+    id = models.CharField(max_length=4, null=False, unique=True)
+    mac_24 = models.ForeignKey(MACAddress, related_name='mac_for_24_ghz', on_delete=models.PROTECT)
+    ip_address_24 = models.CharField(max_length=15)
+    mac_50 = models.ForeignKey(MACAddress, related_name='mac_for_50_ghz', on_delete=models.PROTECT)
+    ip_address_50 = models.CharField(max_length=15)
+
+class PacketReceipt(models.Model):
+    timestamp = models.
+    mac_address = models.ForeignKey(MACAddress, on_delete=models.PROTECT)
+    router = models.ForeignKey(Router, on_delete=models.PROTECT)
+    rss = models.SmallIntegerField()
