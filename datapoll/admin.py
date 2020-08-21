c4d212927a135b96a245b79ea87434cde13c5c69
ChrisHsu <chris.w.hsu@gmail.com>
2020-08-21 15:08:54 -0700

    admin development.

--- datapoll/admin.py
+++ datapoll/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from datapoll.models import *
+
 
 # Register your models here.
+
+class MACAddressAdmin(admin.ModelAdmin):
+    list_display = ['mac_address']
+
+
+admin.site.register(MACAddress, MACAddressAdmin)
+
+
+class MACAdressInline(admin.TabularInline):
+    model = MACAddress
+
+
+class WirelessDeviceAdmin(admin.ModelAdmin):
+    list_display = ['mac_address', 'ip_address', 'hostname', 'alternative_name']
